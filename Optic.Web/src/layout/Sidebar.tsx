6cdd08954506471e427047432ada70990cea3e19
James Bravo <jameszuniga348@gmail.com>
2024-11-07 21:24:15 -0500

    Modificando el Sidebar de inicio y de clientes.

--- Optic.Web/src/layout/Sidebar.tsx
+++ Optic.Web/src/layout/Sidebar.tsx
@@ -1,18 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 export const Sidebar = () => {
     return (
-        <div id="sidebar" className="w-28 bg-gray-300 flex flex-col justify-between">
+        <div id="sidebar" className="w-1/6 bg-white h-screen p-4 bg-gray-100 border-r">
             <nav>
-                <ul>
-                    <li>
-                        <a href={`/contacts/1`} className="w-auto block text-center align mx-1.5 my-1.5 bg-slate-500 px-1.5 py-1 rounded-md">
-                        <img src="https://placehold.co/100x100?text=Icono" alt="logo" className="h-full rounded-full" />
-                        <span>Inicio</span>
+                <ul className="space-y-1">
+                    <li className="flex flex-col items-center rounded-lg p-1">
+                        <a href={`/contacts/1`} className="flex flex-col items-center">
+                        <FontAwesomeIcon icon={faHouse} className=" h-20 w-15 flex flex-col items-center rounded-lg p-1" />
+                        <span className="text-center font-bold h-10 mt-">Inicio</span>
                         </a>
                     </li>
-                    <li>
-                        <a href={`/contacts/1`} className="w-auto block text-center align mx-1.5 my-1.5 bg-slate-500 px-1.5 py-1 rounded-md">
-                        <img src="https://placehold.co/100x100?text=Clientes" alt="logo" className="h-full rounded-full" />
-                        <span>Clientes</span>
+
+                    <li className="flex flex-col items-center rounded-lg p-1 ml-">
+                        <a href={`/contacts/1`} className="flex flex-col items-center">
+                        <FontAwesomeIcon icon={faUser}  className=" h-20 w-15 flex flex-col items-center rounded-lg p-1" />
+                        <span className="text-center font-bold h-10 mt-">Clientes</span>
                         </a>
                     </li>
                 </ul>
