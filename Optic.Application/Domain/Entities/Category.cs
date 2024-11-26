89e9b97ac0fe8fb6c966a9dbf7c328c1999fda98
Deimer Andres Nuñez Novoa <deandrenn@hotmail.com>
2024-11-25 21:02:04 -0500

    Add categories entities

---
+++ Optic.Application/Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using Optic.Application.Domain.Primitives;
+
+namespace Optic.Application.Domain.Entities;
+public class Category : AggregateRoot
+{
+    public Category(int id, int number, string name) : base(id)
+    {
+        Number = number;
+        Name = name;
+    }
+
+    public int Number { get; private set; }
+    public string Name { get; private set; }
+}
