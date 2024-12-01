5452b165d662badc98b59d473919b7352c90454e
Deimer Andres Nuñez Novoa <deandrenn@hotmail.com>
2024-12-01 16:25:01 -0500

    Migrate database relation clients and IdentificationType

--- Optic.Application/Infrastructure/Sqlite/Configurations/IdentificationTypeConfiguration.cs
+++ Optic.Application/Infrastructure/Sqlite/Configurations/IdentificationTypeConfiguration.cs
@@ -11,10 +11,10 @@ public class IdentificationTypeConfiguration : IEntityTypeConfiguration<Identifi
         builder.ToTable("IdentificationTypes");
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Id).ValueGeneratedNever();
-        //builder.HasMany(x => x.Clients)
-        //        .WithOne(x => x.IdentificationType)
-        //        .HasForeignKey(x => x.IdentificationTypeId)
-        //        .IsRequired(false);
+        builder.HasMany(x => x.Clients)
+               .WithOne(x => x.IdentificationType)
+               .HasForeignKey(x => x.IdentificationTypeId)
+               .IsRequired(false);
     }
 }
 
