35010bd021612676b0d512de88ce2613af0327ad
James Bravo <jameszuniga348@gmail.com>
2025-02-03 11:09:44 -0500

    The function of creating and updating brands was created

--- Optic.Web/src/slices/Settings/BrandsForm.tsx
+++ Optic.Web/src/slices/Settings/BrandsForm.tsx
@@ -1,33 +1,14 @@
-import { useEffect, useState } from "react";
+
 import { useBrands, useListSettings } from "../../shared/components/List/useListSettings";
 export const BrandsForm = ({ id }: { id?: number }) => {
     const { settings } = useListSettings();
     const { updateBrand, createBrand } = useBrands();
-    const [form, setForm] = useState<{ name: string }>({ name: "" });
-    useEffect(() => {
-        if (id) {
-            const brand = settings?.brands?.find((brand) => brand.id === id);
-            if (brand) {
-                setForm({ name: brand.name });
-            }
-        }
-    }, [id, settings]);
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    };
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (id) {
-            updateBrand.mutate({ id, ...form });
-        }
-    };
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <input
                     name="name"
-                    value={form.name}
-                    onChange={handleChange}
                     placeholder="Marcas"
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
