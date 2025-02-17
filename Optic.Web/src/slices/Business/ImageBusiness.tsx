44c3b5c20a0fba1ac6cd4d595bfa211edca4cf5e
James Bravo <jameszuniga348@gmail.com>
2025-02-17 12:59:28 -0500

    The business image component was created

---
+++ Optic.Web/src/slices/Business/ImageBusiness.tsx
@@ -0,0 +1,46 @@
+import { faCamera } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react";
+export const ImageBusiness = () => {
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
+   
+    const habdleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            setImagePreview(URL.createObjectURL(file));
+        }
+    };
+    
+    return (
+        <div className=" relative bottom-40 left-16 ">
+            <div className="bg-whit p-6 rounded-lg shadow-lg text-center">
+                <label
+                    htmlFor="file-upload"
+                    className="block border-2 border-dashed border-gray-500 rounded-lg p-1 cursor-pointer hover:border-blue-500 m-4 w-60 h-30"
+                >
+                    <div className="">
+                        {imagePreview ? (
+                            <img
+                                src={imagePreview}
+                                alt="Previsualización"
+                                className="w-60 h-40 object-cover rounded-lg" />
+                        ) : (
+                            <>
+                                <p className="text-gray-600 text-sm">o haz clic para seleccionar</p>
+                            </>
+                        )}
+                    </div>
+                </label>
+                <input
+                    id="file-upload"
+                    type="file"
+                    className="hidden"
+                    accept="image/*"
+                    onChange={habdleImageChange} />
+                <button className=" bg-teal-500 text-white px-4 py-3 rounded-md shadow-md mr-4 hover:bg-teal-400 ">
+                    <FontAwesomeIcon icon={faCamera} className="" />
+                </button>
+            </div>
+        </div>
+    )
+}
