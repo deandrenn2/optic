ee8afd7d96f5339a65fd52f15bd19e5eb726cf8a
James Bravo <jameszuniga348@gmail.com>
2025-03-30 11:29:01 -0500

    The user profiles for updating and changing passwords, clientcard, product.card, and formulacard were modified.

--- Optic.Web/src/slices/Users/ChangePassword.tsx
+++ Optic.Web/src/slices/Users/ChangePassword.tsx
@@ -80,13 +80,15 @@ const PasswordModel = ({ onClose }: { onClose: () => void }) => {
                             onChange={handleChange}
                             className="w-full px-4 py-2 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
                         />
+                        <div className="relative">
                         <button
                             type="button"
-                            className="absolute right-3 top-9 transform -translate-y-1/2 text-gray-600"
-                            onClick={() => setShowPassword(!showPassword)}
-                        >
+                            className="absolute right-3  transform -translate-y-8 text-gray-600"
+                            onClick={() => setShowPassword(!showPassword)}>
                             <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
                         </button>
+                        </div>
+                        
                     </div>
 
                     {/* Confirmar contraseña */}
@@ -99,13 +101,15 @@ const PasswordModel = ({ onClose }: { onClose: () => void }) => {
                             onChange={handleChange}
                             className="w-full px-4 py-2 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
                         />
+                        <div className="relative">
                         <button
                             type="button"
-                            className="absolute right-3 top-9 transform -translate-y-1/2 text-gray-600"
-                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                        >
+                            className="absolute right-3 transform -translate-y-8 text-gray-600"
+                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
                             <FontAwesomeIcon icon={showConfirmPassword ? faEyeSlash : faEye} />
                         </button>
+                        </div>
+                       
                     </div>
 
                     {/* Mensaje de error */}
